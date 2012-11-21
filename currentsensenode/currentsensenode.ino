309bc127ba9b2be0b64d220401d524caabbd4df4
Chris Evich <cevich@redhat.com>
2012-11-21 07:42:38 -0500

    Updated to use Debug macro

--- currentsensenode/currentsensenode.ino
+++ currentsensenode/currentsensenode.ino
@@ -21,12 +21,12 @@
 /*
     Dependencies:
     ebl-arduino: http://code.google.com/p/ebl-arduino/
-    virtual wire: http://www.pjrc.com/teensy/td_libs_VirtualWire.html
+    virtual wire: http://www.open.com.au/mikem/arduino/
 */
 
 #include <TimedEvent.h>
 #include <VirtualWire.h>
-#include "RoboVac.h"
+#include <RoboVac.h>
 
 /* Externals */
 
@@ -70,6 +70,22 @@ int threshold = 0;
 unsigned long overrideTime = 0; // millis when manual override expires
 
 /* Functions */
+
+void txEvent(TimerInformation *Sender) {
+    if (thresholdBreached()) {
+        digitalWrite(txEnablePin, HIGH);
+        makeMessage(&message, byte(NODEID));
+        vw_send((uint8_t *) &message, MESSAGESIZE);
+        PRINTMESSAGE(millis(), message, 0);
+    } else {
+        digitalWrite(txEnablePin, LOW);
+    }
+
+    // Check override button during slow event
+    if (digitalRead(overridePin) == HIGH) {
+        incOverrideTime();
+    }
+}
 
 void updateCurrentEvent(TimerInformation *Sender) {
     char index=0;
@@ -117,45 +133,27 @@ void incOverrideTime(void) {
     if (overrideTime > currentTime) {
         // Still override time in timer
         overrideTime += (OVERRIDEAMOUNT);
-#ifdef DEBUG
-        Serial.print("Override +: ");Serial.println(OVERRIDEAMOUNT);
-#endif // DEBUG
+        D("Override +: ");D(OVERRIDEAMOUNT);
     } else if (overrideTime < currentTime) { // less than currentMillis
         // First press
         overrideTime = currentTime + (OVERRIDEAMOUNT);
-#ifdef DEBUG
-        Serial.print("Override by: ");Serial.println(OVERRIDEAMOUNT);
-#endif // DEBUG
+        D("Override by: ");D(OVERRIDEAMOUNT);
     }
-}
-
-void txEvent(TimerInformation *Sender) {
-    if (thresholdBreached()) {
-        digitalWrite(txEnablePin, HIGH);
-        makeMessage(&message, NODEID);
-        vw_send((uint8_t *) &message, MESSAGESIZE);
-    } else {
-        digitalWrite(txEnablePin, LOW);
-    }
-
-    // Check override button during slow event
-    if (digitalRead(overridePin) == HIGH) {
-        incOverrideTime();
-    }
+    D("\n");
 }
 
 void printStatusEvent(TimerInformation *Sender) {
     unsigned long currentTime = millis();
 
     PRINTTIME(currentTime);
-    Serial.print(" \t threshold: "); Serial.print(threshold);
-    Serial.print("  SampleHigh: "); Serial.print(sampleHigh);
-    Serial.print("  SampleLow: "); Serial.print(sampleLow);
-    Serial.print("  SampleRange: "); Serial.print(sampleRange);
+    D(" \t threshold: "); D(threshold);
+    D("  SampleHigh: "); D(sampleHigh);
+    D("  SampleLow: "); D(sampleLow);
+    D("  SampleRange: "); D(sampleRange);
     if (overrideTime > currentTime) {
-        Serial.print("  Override: "); Serial.print(overrideTime - currentTime);
+        D("  Override: "); D(overrideTime - currentTime);
     }
-    Serial.println("");
+    D("\n");
 }
 
 /* Main Program */
@@ -192,7 +190,7 @@ void setup() {
     if (analogReadMicroseconds > MICROSPERSAMPLE) {
         while (1) {
 #ifdef DEBUG
-            Serial.print("Error: AnalogRead speed too slow!");
+            D("Error: AnalogRead speed too slow!");
 #else
             digitalWrite(statusLEDPin, HIGH);
             delay(250);
@@ -209,24 +207,21 @@ void setup() {
     // Setup events
     TimedEvent.addTimer(SENSEINTERVAL, updateCurrentEvent);
     TimedEvent.addTimer(TXINTERVAL, txEvent);
-
-#ifdef DEBUG
     TimedEvent.addTimer(STATUSINTERVAL, printStatusEvent);
-    Serial.print("setup()"); Serial.print(" Node ID: "); Serial.println(NODEID, DEC);
-    Serial.print("txDataPin: "); Serial.print(txDataPin);
-    Serial.print("  txEnablePin: "); Serial.print(txEnablePin);
-    Serial.print("  statusLEDPin: "); Serial.print(statusLEDPin);
-    Serial.print("  currentSensePin: "); Serial.print(currentSensePin);
-    Serial.print("  overridePin: "); Serial.print(overridePin);
-    Serial.print("  RXTXBAUD: "); Serial.print(RXTXBAUD);
-    Serial.println();
-    Serial.print("Threshold Limit: "); Serial.print(THRESHOLDLIMIT);
-    Serial.print("  Smp. Per. Intvl: "); Serial.print(SAMPLESPERWAVE);
-    Serial.print("  Smp. Duration: "); Serial.print(MICROSPERSAMPLE);
-    Serial.print("us  analogRead: ");
-    Serial.print(analogReadMicroseconds);
-    Serial.println("us");
-#endif // DEBUG
+    D("setup()"); D(" Node ID: "); D(NODEID, DEC);
+    D("\ntxDataPin: "); D(txDataPin);
+    D("  txEnablePin: "); D(txEnablePin);
+    D("  statusLEDPin: "); D(statusLEDPin);
+    D("  currentSensePin: "); D(currentSensePin);
+    D("  overridePin: "); D(overridePin);
+    D("  RXTXBAUD: "); D(RXTXBAUD);
+    D("\n");
+    D("Threshold Limit: "); D(THRESHOLDLIMIT);
+    D("  Smp. Per. Intvl: "); D(SAMPLESPERWAVE);
+    D("  Smp. Duration: "); D(MICROSPERSAMPLE);
+    D("us  analogRead: ");
+    D(analogReadMicroseconds);
+    D("us\n");
 }
 
 void loop() {
